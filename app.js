8d5a8a561a5dba03fb37fbb2e74ffd1d4f1c2751
Ubong James <ubjames001@gmail.com>
2020-10-29 20:22:32 +0100

    changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { apiKey as weather_provider } from "./dateTime.js";
 const menu = document.querySelector("nav");
 const menuIcon = document.querySelector(".fa-bars");
 const exitMenu = document.querySelector(".fa-angle-left");
@@ -15,9 +16,32 @@ const maxTemp = document.querySelector(".max-temp");
 const minTemp = document.querySelector(".min-temp");
 const tempDescription = document.querySelector(".temp-description");
 const currentLocation = document.querySelector(".location");
-// const icon = document.querySelector("#icon");
-// const icon = new Skycons({ color: "green" });
+const icon = document.querySelector("#icon");
+const shareOn = document.querySelector(".floatBtn");
+const share = document.querySelector(".dev-gp-2");
+let timeout;
+share.addEventListener("mouseover", (e) => {
+  shareOn.classList.add("share-on");
+  shareOn.style.display = "flex";
+  if ((shareOn.style.transfrom = "translate(-1%)")) {
+    timeout = setTimeout(() => {
+      shareOn.style.zIndex = 1;
+    }, 100);
+  }
+});
 
+shareOn.addEventListener("mouseleave", () => {
+  shareOn.style.zIndex = -1;
+  clearTimeout(timeout);
+
+  setTimeout(() => {
+    shareOn.classList.remove("share-on");
+  }, 80);
+
+  setTimeout(() => {
+    shareOn.style.display = "none";
+  }, 400);
+});
 menuIcon.addEventListener("click", () => {
   menu.classList.add("active-nav");
 });
@@ -33,7 +57,7 @@ search.addEventListener("blur", () => {
   search.classList.remove("active-search");
 });
 
- onBackground();
+onBackground();
 containerSwitch.classList.add("inactive");
 modeSwitch.classList.add("inactive");
 let thereIsBkgd;
@@ -53,9 +77,7 @@ bkgdImage.addEventListener("click", () => {
     containerSwitch.classList.add("inactive");
     modeSwitch.classList.add("inactive");
   }
-}); 
-
-
+});
 
 function onBackground() {
   body.style.background =
@@ -68,6 +90,7 @@ function onBackground() {
   menu.style.color = "#000";
   search.style.borderBottom = "1px solid #fff";
   search.style.color = "#fff";
+  shareOn.style.background = "#cccccc";
 }
 
 function dark() {
@@ -77,6 +100,7 @@ function dark() {
   header.style.background = "#111111";
   menu.style.color = "#fff";
   menu.style.background = "rgb(31, 31, 31)";
+  shareOn.style.background = "rgb(31, 31, 31)";
   search.style.borderBottom = "1px solid #fff";
   search.style.color = "#fff";
 }
@@ -88,6 +112,7 @@ function light() {
   menu.style.color = "#000";
   header.style.background = "#ccccccb7";
   menu.style.background = "#cccccc";
+  shareOn.style.background = "#cccccc";
   search.style.color = "#000";
   search.style.borderBottom = "1px solid #000";
 }
@@ -103,12 +128,12 @@ const KELVIN = 273;
 let tempMax;
 let tempMin;
 
-const apiKey = "2ef5416b6e4369b6f731e854324a26be";
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
   alertBox.style.visibility = "visible";
-  alertBox.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+  alertBox.innerHTML =
+    "<p>Browser doesn't Support Geolocation, please upgrade your browser and try again</p>";
 }
 
 // SET USER'S POSITION
@@ -121,48 +146,86 @@ function setPosition(position) {
 
 // SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
 function showError(error) {
+  let noInternet = "You're not connected";
   alertBox.style.visibility = "visible";
-  alertBox.innerHTML = `<p> ${error.message} </p>`;
+
+  if (error.code == 2) {
+    alertBox.innerHTML = `<p> ${noInternet} </p>`;
+  } else {
+    alertBox.innerHTML = `<p> ${error.message} </p>`;
+  }
 }
 
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude) {
-  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weather_provider}`;
 
   fetch(api)
     .then((response) => {
       let data = response.json();
-      console.log(data);
       return data;
     })
     .then((data) => {
-      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-      tempMax = Math.floor(data.main.temp_max - KELVIN);
-      tempMin = Math.floor(data.main.temp_min - KELVIN);
-      weather.description = data.weather[0].description;
-      weather.iconId = data.weather[0].icon;
-      weather.city = data.name;
-      weather.country = data.sys.country;
+      configData(data);
     })
     .then(() => {
       displayWeather();
     });
 }
+
+function configData(data) {
+  weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+  tempMax = Math.floor(data.main.temp_max - KELVIN);
+  tempMin = Math.floor(data.main.temp_min - KELVIN);
+  weather.description = data.weather[0].description;
+  weather.iconId = data.weather[0].icon;
+  weather.city = data.name;
+  weather.country = data.sys.country;
+  weather.wind = data.wind.speed;
+  weather.pressure = data.main.pressure;
+  weather.feelsLike = Math.floor(data.main.feels_like - KELVIN);
+  weather.seaLevel = data.main.sea_level;
+  weather.humidity = data.main.humidity;
+}
+
 function displayWeather() {
-  temperature.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-  //   icon.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+  let fahUnit = document.querySelector("#fahrinheit");
+  setInterval(() => {
+    if (fahUnit.checked) {
+      let toFahrinheit = Math.floor((9 / 5) * weather.temperature.value + 35);
+      temperature.innerHTML = `${toFahrinheit}°<span class="SmallC">F</span>`;
+    } else {
+      temperature.innerHTML = `${weather.temperature.value}°<span class="SmallC">C</span>`;
+    }
+  }, 100);
+
+  icon.innerHTML = `<img src="SVGs/${weather.iconId}.svg" width="300"/>`;
+  console.log(weather.iconId);
   tempDescription.innerHTML = weather.description;
   currentLocation.innerHTML = `${weather.city}, ${weather.country}`;
   maxTemp.innerHTML = `${tempMax}°<span>C</span>`;
   minTemp.innerHTML = `${tempMin}°<span>C</span>`;
-  icon.set("icon", icon.PARTLY_CLOUDY_DAY);
-  icon.play();
+  //
+  console.log(weather.humidity);
+  document.querySelector("#wind").innerHTML = `${weather.wind} mph`;
+  document.querySelector("#feels-like").innerHTML = ` ${weather.feelsLike}°`;
+  document.querySelector("#pressure").innerHTML = `${weather.pressure}`;
+  document.querySelector("#humidity").innerHTML = `${weather.humidity}%`;
+  document.querySelector("#sea-level").innerHTML = `${weather.seaLevel}`;
 }
 
-// let clickin = menu.addEventListener('click', (e)=>console.log(e))
-// if(!clickin){
-//   console.log('out')
-// }
+//search location
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let inputVal = search.value;
+
+  const weatherProvider = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weather_provider}`;
+  fetch(weatherProvider)
+    .then((res) => res.json())
+    .then((data) => configData(data))
+    .then(() => displayWeather());
+});
 
 document.addEventListener("click", (e) => {
   let scrnWidth = e.clientX;
