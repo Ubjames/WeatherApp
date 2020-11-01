ba7335a2249e9dd16f933b770c9694f77acf8017
Ubong James <ubjames001@gmail.com>
2020-11-01 23:50:40 +0100

    changes

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ function showError(error) {
 
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude) {
-  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weather_provider}`;
+  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weather_provider}`;
 
   fetch(api)
     .then((response) => {
